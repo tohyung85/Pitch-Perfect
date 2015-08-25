3ce31f1e5218ad07b15521c38cc999ee4f9e7dcf
Joshua <tantohyung@gmail.com>
2015-08-25 20:57:30 +0800

    Hide stop button on start up and for it to appear only on record commencement

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var recordInProcess: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         recordInProcess.hidden = true
+        stopButton.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +28,12 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Record User's Voice
         recordInProcess.hidden = false
+        stopButton.hidden = false
     }
 
     @IBAction func stopRecording(sender: UIButton) {
         recordInProcess.hidden = true
+        stopButton.hidden = true
     }
 }
 
