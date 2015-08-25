30e7ec388611574c0d6b973a29cbb16f59dca5f3
Joshua <tantohyung@gmail.com>
2015-08-25 20:28:58 +0800

    Fix label constraints and set default visibility as hidden

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var recordInProcess: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        recordInProcess.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
