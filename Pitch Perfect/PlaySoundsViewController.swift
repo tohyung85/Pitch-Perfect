4b8bb7ab4e3c5931a7d2f30666cde9689fd7f2ca
Joshua <tantohyung@gmail.com>
2015-08-26 22:25:25 +0800

    Add snail button and play functionality for movie quote

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Joshua  Tan on 26/8/15.
+//  Copyright (c) 2015 Joshua. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var url = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: url, error: nil)
+                    println(url)
+        } else {
+            println("Invalid File Path")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowSound(sender: UIButton) {
+        audioPlayer.play()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
